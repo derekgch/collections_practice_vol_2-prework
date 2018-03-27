0b239f1e6e34d2fe7106673777be41d5be91b5df
derek chen <derek.g.chen@gmail.com>
2018-03-27 17:42:41 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -105,12 +105,12 @@ def merge_data(k,v)
   merged = []
   k.each do |name|
     v.each do |data|
-      puts "keys #{data.keys} + #{name.values}"
-      if data.keys == name.values
+    puts "keys #{data.keys} + #{name.values}"
+      
 
-        temp = name.value.merge(data.value)
+        temp = name.value.merge(data[name.values]
         merged << temp
-      end #if
+    
     end #v
   end #k
   merged
